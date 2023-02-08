e390297891da7c7e527365f96011fc13efe207a8
RuteSantos <1160663@isep.ipp.pt>
2023-02-08 22:15:01 +0000

    include call of health API for the web services santos, subscribe and query

---
+++ internal/http/healthcheck.go
@@ -0,0 +1,75 @@
+package http
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/palavrapasse/monitor/internal/logging"
+	"github.com/palavrapasse/paramedic/pkg"
+)
+
+func GetSantosHealth() (pkg.HealthStatus, error) {
+	return getServiceHealth(SantosServiveHealthURL)
+}
+
+func GetQueryHealth() (pkg.HealthStatus, error) {
+	return getServiceHealth(QueryServiveHealthURL)
+}
+
+func GetSubscribeHealth() (pkg.HealthStatus, error) {
+	return getServiceHealth(SubscribeServiveHealthURL)
+}
+
+func getServiceHealth(url string) (pkg.HealthStatus, error) {
+	response, err := httpGetCallService(url)
+
+	if err != nil {
+		return pkg.HealthStatus{}, err
+	}
+
+	return toHealthStatus(response)
+}
+
+func httpGetCallService(url string) (*http.Response, error) {
+
+	logging.Aspirador.Trace(fmt.Sprintf("Calling web service: %s", url))
+
+	resp, err := http.Get(url)
+
+	if err != nil {
+		logging.Aspirador.Error(fmt.Sprintf("Error while calling web Service: %s", err))
+		return nil, err
+	}
+
+	responseStatus := resp.StatusCode
+	logging.Aspirador.Trace(fmt.Sprintf("Web service response status: %d", responseStatus))
+
+	if responseStatus != http.StatusOK {
+		err = fmt.Errorf("received status code %d from service when calling %s", responseStatus, url)
+	}
+
+	return resp, err
+}
+
+func toHealthStatus(resp *http.Response) (pkg.HealthStatus, error) {
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		logging.Aspirador.Error(fmt.Sprintf("Error while reading body of web service response: %s", err))
+		return pkg.HealthStatus{}, err
+	}
+
+	var data pkg.HealthStatus
+
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		logging.Aspirador.Error(fmt.Sprintf("Error while unmarshal body: %s", err))
+		return pkg.HealthStatus{}, err
+	}
+
+	return data, err
+}
